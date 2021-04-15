9a6e1aeec88d58a7b26d76dda58e2795bb70d66c
Kieran <kieranknowles11@hotmail.co.uk>
2021-04-15 14:51:00 +0100

    database_conn without password

---
+++ database_conn_nopass.php
@@ -0,0 +1,23 @@
+<?php
+require_once "utility.php";
+utility::noDirectAccess();
+
+// Connection can be accessed from $conn
+
+// MySQLi parameters
+$servername = "localhost";
+$username = "unn_w20013000";
+$password = "p9hm*FunVdxb%o";
+$dbname = "unn_w20013000";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_errno) {
+    $error = $conn->connect_error;
+    echo "<p>Connection to database failed</p>";
+    echo "<p>The error was: $error.</p>";
+    exit;
+}
+?>
