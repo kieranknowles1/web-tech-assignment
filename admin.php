7539c447d1a3e944a5d45fabde5db7e7e10038a3
Kieran <kieranknowles11@hotmail.co.uk>
2021-04-20 09:55:52 +0100

    Begin admin page

---
+++ admin.php
@@ -0,0 +1,51 @@
+<?php
+    $title = "Admin - Leading Choice Getaways";
+
+    require "header.php";
+
+    require "database_conn.php";
+    
+    $sql = "SELECT holidayID, holidayTitle
+                FROM LCG_holidays
+                ORDER BY holidayTitle";
+    
+    $queryResult = utility::query($conn, $sql);
+?>
+<label>Edit Holiday: 
+<select name="editHoliday" onchange="location = this.value;">
+    <option selected style="display:none">Select a Holiday</option>
+    <?php
+        // https://stackoverflow.com/questions/2000656/using-href-links-inside-option-tag
+        while ($row = $queryResult->fetch_object()) {
+            echo "\t<option value='editHoliday.php?id=$row->holidayID'>$row->holidayTitle</option>\n";
+        }
+    ?>
+</select>
+</label><br>
+
+<?php
+    $sql = "SELECT catID, catDesc
+                FROM LCG_category
+                ORDER BY catDesc";
+    
+    $queryResult = utility::query($conn, $sql);
+?>
+
+<label>Edit Category: 
+<select name="editCategory" onchange="location = this.value;">
+    <option selected style="display:none">Select a Category</option>
+    <?php
+        while ($row = $queryResult->fetch_object()) {
+            echo "\t<option value='editHoliday.php?id=$row->catID'>$row->catDesc</option>\n";
+        }
+    ?>
+</select>
+</label><br>
+
+<a href="newHoliday.php">New Holiday</a><br>
+<a href="newCategory.php">New Category</a>
+
+
+<?php
+    require "footer.php";
+?>
---
+++ template.php
@@ -0,0 +1,8 @@
+<?php
+    $title = "";
+
+    require "header.php";
+?>
+<?php
+    require "footer.php";
+?>
