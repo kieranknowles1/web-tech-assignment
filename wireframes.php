56e90365001591d8085883d170146c1ce800c68f
Kieran <kieranknowles11@hotmail.co.uk>
2021-04-28 10:09:39 +0100

    Fix wireframes page

--- wireframes.php
+++ wireframes.php
@@ -1,11 +1,11 @@
 <?php
     $title = "Wireframes - Leading Choice Getaways";
-    include_once "header.php";
+    include_once "lib/header.php";
 ?>
 
 <a href="wireframes/desktop.pdf" download>Desktop</a><br>
 <a href="wireframes/mobile.pdf" download>Mobile</a>
 
 <?php
-    include_once "footer.php";
+    include_once "lib/footer.php";
 ?>
