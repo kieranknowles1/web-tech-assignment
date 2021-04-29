dac3d3d6b762a5281b44afd7a80a8509d4edc00f
Kieran <kieranknowles11@hotmail.co.uk>
2021-04-29 14:28:07 +0100

    Improved admin page layout

--- admin/index.php
+++ admin/index.php
@@ -7,66 +7,65 @@
 
     require "$root/lib/database_conn.php";
 ?>
-<label>Edit Holiday: 
-<select name="editHoliday" onchange="location = this.value;">
-    <option selected style="display:none">Select a Holiday</option>
-    <?php
-        $holidaySql = "SELECT holidayID, holidayTitle
-                       FROM LCG_holidays
-                       ORDER BY holidayTitle";
+<table class="adminTable">
+    <tr>
+        <td><label for="editHoliday">Edit Holiday: </label></td>
+        <td><select name="editHoliday" id="editHoliday" class="adminSelect" onchange="location = this.value;">
+            <option selected style="display:none">Select a Holiday</option>
+            <?php
+                $holidaySql = "SELECT holidayID, holidayTitle
+                            FROM LCG_holidays
+                            ORDER BY holidayTitle";
 
-        $holidayQuery = utility::query($holidaySql);
+                $holidayQuery = utility::query($holidaySql);
 
-        // https://stackoverflow.com/questions/2000656/using-href-links-inside-option-tag
-        while ($row = $holidayQuery->fetch_object()) {
-            echo "\t<option value='editHoliday.php?id=$row->holidayID'>$row->holidayTitle</option>\n";
-        }
-    ?>
-</select>
-</label><br>
+                // https://stackoverflow.com/questions/2000656/using-href-links-inside-option-tag
+                while ($row = $holidayQuery->fetch_object()) {
+                    echo "\t<option value='editHoliday.php?id=$row->holidayID'>$row->holidayTitle</option>\n";
+                }
+            ?>
+        </select></td>
+        <td><a href="editHoliday.php">New</a></td>
+    </tr>
 
-<?php
+    <tr>
+        <td><label for="editCategory">Edit Category: </label></td>
+        <td><select name="editCategory" id="editCategory" class="adminSelect" onchange="location = this.value;">
+            <option selected style="display:none">Select a Category</option>
+            <?php
+                $categorySql = "SELECT catID, catDesc
+                                FROM LCG_category
+                                ORDER BY catDesc";
 
-?>
+                $categoryQuery = utility::query($categorySql);
 
-<label>Edit Category: 
-<select name="editCategory" onchange="location = this.value;">
-    <option selected style="display:none">Select a Category</option>
-    <?php
-        $categorySql = "SELECT catID, catDesc
-                        FROM LCG_category
-                        ORDER BY catDesc";
+                while ($row = $categoryQuery->fetch_object()) {
+                    echo "\t<option value='editCategory.php?id=$row->catID'>$row->catDesc</option>\n";
+                }
+            ?>
+        </select></td>
+        <td><a href="editCategory.php">New</a></td>
+    </tr>
 
-        $categoryQuery = utility::query($categorySql);
+    <tr>
+        <td><label for="editLocation">Edit Location: </label></td>
+        <td><select name="editLocation" id="editLocation" class="adminSelect" onchange="location = this.value;">
+            <option selected style="display:none">Select a Location</option>
+            <?php
+                $locationSql = "SELECT locationID, locationName, country
+                                FROM LCG_location
+                                ORDER BY locationName";
+                
+                $locationQuery = utility::query($locationSql);
 
-        while ($row = $categoryQuery->fetch_object()) {
-            echo "\t<option value='editCategory.php?id=$row->catID'>$row->catDesc</option>\n";
-        }
-    ?>
-</select>
-</label><br>
-
-<label>Edit Location:
-<select name="editLocation" onchange="location = this.value;">
-    <option selected style="display:none">Select a Location</option>
-    <?php
-        $locationSql = "SELECT locationID, locationName, country
-                        FROM LCG_location
-                        ORDER BY locationName";
-        
-        $locationQuery = utility::query($locationSql);
-
-        while ($row = $locationQuery->fetch_object()) {
-            echo "\t<option value='editLocation.php?id=$row->locationID'>$row->locationName, $row->country</option>\n";
-        }
-    ?>
-</select>
-</label><br>
-
-<a href="editHoliday.php">New Holiday</a><br>
-<a href="editCategory.php">New Category</a><br>
-<a href="editLocation.php">New Location</a>
-
+                while ($row = $locationQuery->fetch_object()) {
+                    echo "\t<option value='editLocation.php?id=$row->locationID'>$row->locationName, $row->country</option>\n";
+                }
+            ?>
+        </select></td>
+        <td><a href="editLocation.php">New</a></td>
+    </tr>
+</table>
 
 <?php
     require "$root/lib/footer.php";
